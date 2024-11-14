5f8c452aa3091fea836fc0932d13597e9563eea4
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-11-14 13:13:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import Navbar from './components/Layout/navbar';
+import { darkTheme } from './theme';
 
 
 function App() {
   return (
-    <Router >
-      <Navbar />
-      <Box className="content">
-        {/* Čia pridėkite savo Routes ir Route komponentus */}
-        <Routes>
-          {/* pvz., <Route path="/" element={<Home />} /> */}
-        </Routes>
-      </Box>
-    </Router>
+   <ThemeProvider theme={darkTheme}> {/* Naudojame tamsią temą */}
+      <CssBaseline /> {/* Užtikriname, kad tema būtų taikoma visam puslapiui */}
+      <Router>
+        <Navbar />
+        <Box className="content">
+          <Routes>
+            {/* Čia galite pridėti savo routes */}
+            {/* pvz., <Route path="/" element={<Home />} /> */}
+          </Routes>
+        </Box>
+      </Router>
+    </ThemeProvider>
   );
 }
 
