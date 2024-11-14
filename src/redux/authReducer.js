19536030f34be0c368b7fd0b0ba22a3514cfed98
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-11-14 12:42:16 +0200

    Update authReducer.js

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,31 +1,50 @@
+// authReducer.js
 const initialState = {
-    user: null,
     isAuthenticated: false,
-    loading: false,
+    user: null,
     error: null,
+    loading: false,
 };
 
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'LOGIN_REQUEST':
+        case 'REGISTER_REQUEST':
+        case 'LOGOUT_REQUEST':
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
         case 'LOGIN_SUCCESS':
         case 'REGISTER_SUCCESS':
             return {
                 ...state,
-                user: action.payload,
                 isAuthenticated: true,
+                user: action.payload,
+                loading: false,
+                error: null,
+            };
+        case 'LOGOUT_SUCCESS':
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null,
                 loading: false,
                 error: null,
             };
         case 'LOGIN_FAILURE':
         case 'REGISTER_FAILURE':
+        case 'LOGOUT_FAILURE':
             return {
                 ...state,
-                error: action.payload,
                 loading: false,
+                error: action.payload,
             };
         default:
             return state;
     }
 };
+
 
 export default authReducer;
